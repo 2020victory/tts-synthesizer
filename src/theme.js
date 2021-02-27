2f4e3e6d9efce2f5413bcc0b09035d82306aa3d1
Shivani R <65798746+shivu-srk@users.noreply.github.com>
2021-02-27 18:40:50 +0530

    colors changed

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import {yellow, purple} from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+    palette: {
+      secondary: {
+        main: '#d81b60',
+      },
+      primary: {
+        main: '#5c6bc0',
+      },
+    },
+  });
+
+export default theme;
