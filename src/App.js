981ca63ee22ac67a79bd3d506bb93a99717b19de
Shivani R <65798746+shivu-srk@users.noreply.github.com>
2021-02-27 17:53:36 +0530

    text field component modified

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import {Grid, AppBar, Toolbar, Typography, TextField, IconButton} from '@material-ui/core';
-import GetAppRoundedIcon from '@material-ui/icons/GetAppRounded';
-import PlayCircleOutlineRoundedIcon from '@material-ui/icons/PlayCircleOutlineRounded';
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GraphicEqIcon from "@material-ui/icons/GraphicEq";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
 
   return (
     <>
-    <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h5">Text To Speech Synthesizer</Typography>
-    </Toolbar>
-    </AppBar>
-    <Grid container justify="center" alignItems="center" direction="column" style={{ minHeight: "70vh"}}>
-    <Grid item>
-      <TextField id="outlined-basic" label="Text Input" variant="outlined" color="primary" />
-    </Grid>
-    <Grid item>
-    <IconButton color="primary" aria-label="download">
-      <GetAppRoundedIcon />
-    </IconButton>
-    <IconButton color="secondary" aria-label="play">
-      <PlayCircleOutlineRoundedIcon />
-    </IconButton>
-    </Grid>
-    </Grid>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">Text To Speech Synthesizer</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+        style={{ minHeight: "70vh" }}
+      >
+        <Grid item>
+          <TextField
+            id="outlined-multiline-static"
+            label="Text Input"
+            multiline
+            rows={4}
+            defaultValue=""
+            variant="outlined"
+            color="secondary"
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            startIcon={<GraphicEqIcon />}
+          >
+            Generate Audio
+          </Button>
+        </Grid>
+      </Grid>
     </>
   );
 }
